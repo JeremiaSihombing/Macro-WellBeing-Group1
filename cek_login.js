61d453850cf215e615d1d3a3138a1718fa92e5fc
fuji-184 <fujisantoso134@gmail.com>
2023-05-01 08:13:05 +0700

    Update Pesan Login

--- cek_login.js
+++ cek_login.js
@@ -1,5 +1,4 @@
-
-  if (!localStorage.getItem('sudah_login')){
-    window.location.replace('login.html');
-  }
-
+if (!localStorage.getItem('sudah_login')){
+  localStorage.setItem('pesan', 'Silahkan masuk ke akun Anda terlebih dahulu. Jika belum punya akun, silahkan mendaftarkan akun terlebih dahulu')
+  window.location.replace('login.html');
+}
