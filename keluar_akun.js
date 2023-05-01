9f2ae18ac5d4c58678a34c54286fa35554ede34a
fuji-184 <fujisantoso134@gmail.com>
2023-05-01 16:39:17 +0700

    Update Pesan Keluar Akun

--- keluar_akun.js
+++ keluar_akun.js
@@ -1,5 +1,6 @@
 document.querySelector('.keluar').addEventListener('click', (e)=>{
   e.preventDefault();
   localStorage.removeItem('sudah_login');
+  localStorage.setItem('pesan', 'Anda berhasil keluar!');
   window.location.replace('index.html');
 });
